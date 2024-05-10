64cf7d66664d784c58b9e5438268d2667c2ae882
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-10 21:25:40 +0200

    add: class main con richieste dall'utente

---
+++ src/org/lessons/java/geometria/Main.java
@@ -0,0 +1,17 @@
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Inserisci la base del rettangolo:");
+		String baseRettangolo = scan.nextLine();
+		
+		System.out.println("Inserisci l'altezza del rettangolo:");
+		String altezzaRettangolo = scan.nextLine();
+		
+		scan.close();
+	}
+}
