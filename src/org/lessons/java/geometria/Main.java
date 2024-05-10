071ce81da8c33b895c9534cae4457a729080d764
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-10 21:44:56 +0200

    add: modificati nomi variabili e invocati metodi finali

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -7,12 +7,17 @@ public class Main {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Inserisci la base del rettangolo:");
-		int baseRettangolo = scan.nextInt();
+		int base = scan.nextInt();
 		
 		System.out.println("Inserisci l'altezza del rettangolo:");
-		int altezzaRettangolo = scan.nextInt();
+		int altezza = scan.nextInt();
 		
+		//istanza della classe rettangolo
+		Rettangolo rettangolo = new Rettangolo(base, altezza);
 		
+		//calcolo finale invocando i metodi
+		System.out.println("L'area del rettangolo corrisponde a" + rettangolo.calcolaArea());
+		System.out.println("Il perimetro del rettangolo corrisponde a" + rettangolo.calcolaPerimetro());		
 		
 		scan.close();
 	}
