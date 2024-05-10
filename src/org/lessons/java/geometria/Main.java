d6dad7018deefe620eb79a86d7765295b1103e75
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-10 21:41:24 +0200

    add: modifica da tipo non primitivo stringa a tipo primitivo intero

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -2,17 +2,17 @@ package org.lessons.java.geometria;
 
 import java.util.Scanner;
 
-import 
-
 public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Inserisci la base del rettangolo:");
-		String baseRettangolo = scan.nextLine();
+		int baseRettangolo = scan.nextInt();
 		
 		System.out.println("Inserisci l'altezza del rettangolo:");
-		String altezzaRettangolo = scan.nextLine();
+		int altezzaRettangolo = scan.nextInt();
+		
+		
 		
 		scan.close();
 	}
