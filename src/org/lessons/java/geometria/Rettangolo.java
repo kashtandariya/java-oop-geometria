8b4355cb06b0ed2e923a454bb50172060c66ed1b
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-10 21:34:44 +0200

    add: classe e costruttore di base

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,5 +1,11 @@
 package org.lessons.java.geometria;
 
 public class Rettangolo {
+	public int base;
+	public int altezza;
 
+	public Rettangolo() {
+		this.base = base;
+		this.altezza = altezza;
+	}
 }
