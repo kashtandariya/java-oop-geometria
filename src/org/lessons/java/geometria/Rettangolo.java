b81a41b1c44d4f6d178deac24899b9a25db8b1f7
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-16 10:29:24 +0200

    add: metodo disegna rettangolo

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -16,4 +16,35 @@ public class Rettangolo {
 	public int calcolaPerimetro() {
 		return 2 * (base+altezza);
 	}
+	
+	/*per disegnare il rettangolo, si fa un ciclo for innestato in un altro ciclo for.
+	Partiamo dall'altezza, nel senso che la prendiamo come punto di partenza e per
+	ogni punto orizzontale dell'altezza all'indice 1 che stiamo seguendo
+	disegniamo un segno deciso da noi per la base oppure inseriamo uno spazio.
+	Mano a mano che andiamo avanti, finiti i punti della base nella riga all'altezza indice 1, andremo alla riga 
+	all'altezza indice 2 e così via fino alla fine.
+	
+	Mettiamo quindi delle condizioni if/else ecc in questi cicli for
+	*/
+	public void disegna () {
+		for (int i = 1; i <= this.altezza); i++) {
+			
+			for (int y = 1; y <= this.base; y++) {
+				
+				if (i == 1 || i == this.altezza) {
+					System.out.println("* ");
+					
+				} else if (y == 1 || y = this.base) {
+					System.out.println("* ");
+					
+				} else {
+					System.out.println("  ");
+				}
+				
+				if (y == this.base) {
+					System.out.println("\n");
+				}
+			}
+		}
+	}
 }
