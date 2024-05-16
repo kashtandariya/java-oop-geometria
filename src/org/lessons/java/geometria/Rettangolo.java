47fceeacfab52181bd88b7f815910bd2bf008503
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-16 10:18:59 +0200

    add: modificatori di visibilità della classe

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,8 +1,8 @@
 package org.lessons.java.geometria;
 
 public class Rettangolo {
-	public int base;
-	public int altezza;
+	private int base;
+	private int altezza;
 
 	public Rettangolo(int base, int altezza) {
 		this.base = base;
